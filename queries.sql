5769ff7c840b458e20a0f7ce57aeb16916e4eb0d
vrynerson <98570777+vrynerson@users.noreply.github.com>
2022-05-15 18:22:42 -0700

    query dates

--- queries.sql
+++ queries.sql
@@ -57,7 +57,7 @@ CREATE TABLE titles (
   	PRIMARY KEY (emp_no),
 	UNIQUE (title)
 );
-
+--this code worked
 CREATE TABLE public.titles
 (
     emp_no integer,
@@ -81,3 +81,50 @@ SELECT * FROM dept_manager;
 SELECT * FROM salaries;
 SELECT * FROM dept_emp;
 SELECT * FROM titles;
+
+-- employees born bw 1952 and 1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- employees born in 1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- employees born in 1954
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+-- employees born in 1953
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+-- employees born in 1952
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- RETIREMENT ELIGIBILITY 
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- create a new table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--display table
+SELECT * FROM retirement_info;
